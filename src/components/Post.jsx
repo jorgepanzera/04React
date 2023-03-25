1592579fd3eb452b9af059e95f35a87ba8f39979
Jorge <panzera.jorge@gmail.com>
2023-03-25 20:18:26 -0300

    columnas para el medio

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,20 @@
-function Post({ title, img, description }) {
+function Post({ img, postDate, user, likes, message, comments }) {
+
+    img = "../assets/img/" + img
+    let myDate = Date(postDate[0],postDate[1], postDate[2,postDate[3],postDate[4]])
+
     return (
       <div className="card m-3" style={{ width: "18rem" }}>
-        <img src={img} className="card-img-top" alt="..." />
+        <img src=`../assets/img${img}` className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{title || "default title"}</h5>
-          <p className="card-text">{description}</p>
+          <h5 className="card-title">{user || "default title"}</h5>
+          <p className="card-text">{message}</p>
         </div>
       </div>
     );
   }
   
   export default Post;
+  
+
   
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,36 +2,43 @@ import Post from "./Post";
 
 const posts = [
   {
-    img: "https://via.placeholder.com/150",
-    title: "Episode 1",
-    description: "Episode 1 description",
+    img: "Avengers.jpg",
+    postDate: [2023,3,10, 11, 52],
+    user: "eric",
+    likes: 43,
+    message: "Los miembros supervivientes de Los Vengadores y sus aliados intentan revertir el daño causado por Thanos en Infinity War.",
+    comments: 15
   },
   {
-    img: "https://via.placeholder.com/150",
-    title: "Episode 2",
-    description: "Episode 2 description",
+    img: "Matrix.jpg",
+    postDate: [2023,3,15, 2, 16],
+    user: "pamela",
+    likes: 35,
+    message: "Matrix es una tetralogía de películas de ciencia ficción escritas y dirigidas por las hermanas Wachowski. Se compone de The Matrix (1999), The Matrix Reloaded (2003), The Matrix Revolutions (2003) y The Matrix Resurrections (2021)",
+    comments: 9
   },
   {
-    img: "https://via.placeholder.com/150",
-    title: "Episode 3",
-    description: "Episode 3 description",
-  },
-  {
-    img: "https://via.placeholder.com/150",
-    title: "Episode 4",
-    description: "Episode 4 description",
-  },
+    img: "Spiderman.jpg",
+    postDate: [2023,3,25, 14, 20],
+    user: "david",
+    likes: 18,
+    message: "Posteriormente a los eventos de Avengers: Endgame,2​3​ y tras 8 meses de la muerte de su mentor Tony Stark, Peter Parker decide pasar unas merecidas vacaciones en Europa junto a sus amigos Ned Leeds y Michelle; pero sus planes al final se ven truncados cuando Nick Fury le encomienda una nueva misión",
+    comments: 12
+  }
 ];
 
 function PostList() {
   return (
-    <div className="d-flex p-5">
-      {posts.map((episode, i) => (
+    <div className="row gx-5 gy-3 mx-auto">
+      {posts.map((post, i) => (
         <Post
           key={i}
-          img={episode.img}
-          title={episode.title}
-          description={episode.description}
+          img={post.img}
+          postDate={post.postDate}
+          user={post.user}
+          likes={post.likes}
+          message={post.message}
+          comments={post.comments}
         />
       ))}
     </div>
