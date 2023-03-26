30053d350a951f14929b27f0d90aebd9de1e48c5
Jorge <panzera.jorge@gmail.com>
2023-03-26 12:25:22 -0300

    ready

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,19 +1,68 @@
-function Post({ img, postDate, user, likes, message, comments }) {
 
-    img = "../assets/img/" + img
-    let myDate = Date(postDate[0],postDate[1], postDate[2,postDate[3],postDate[4]])
+
+
+function Post({ img, age, user, likes, message, comments }) {
+
+    const image = require('../assets/images/' + img)
+    age = age + " ago"
+    user = "@" + user
+    comments = "(" + comments + ")"
+
+   console.log(img)
+   console.log(image)
 
     return (
-      <div className="card m-3" style={{ width: "18rem" }}>
-        <img src=`../assets/img${img}` className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{user || "default title"}</h5>
-          <p className="card-text">{message}</p>
+      <div className="col-12 col-xs-6 col-sm-6 col-md-4 col-lg-3 d-flex">
+        <div class="card">
+
+          <img src={image} class="card-img-top" alt="..." />
+
+          <div class="card-body">
+
+            <div class="d-flex flex-row">
+              <div class="col-9 justify-content-start">
+                <p class="text-muted">{age}</p>
+                <p class="fw-bold">{user}</p>
+              </div>
+              <div class="col-3 d-flex justify-content-end align-items-center">
+                <div class="d-flex likes-div px-1 py-1">
+                  <span class="fa fa-heart"> </span>
+                  <p class="card-text my-0 mx-1">{likes}k</p>
+                </div>
+              </div>
+            </div>
+            
+            <p class="card-text fw-normal mt-2">{message}</p>
+
+            <div class="row d-flex">
+              <div class="col-9 justify-content-start align-items-center">
+                <div class="d-flex">
+                  <span class="fa fa-comment"> </span>
+                  <p class="text-muted mx-2">Comments {comments}</p>
+                </div>
+              </div>
+            </div>  
+
+
+          </div> 
         </div>
       </div>
     );
   }
   
+  
+  
+
+    
+      
+
+        
+   
+      
+
+   
+
+
   export default Post;
   
 
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,39 +2,39 @@ import Post from "./Post";
 
 const posts = [
   {
-    img: "Avengers.jpg",
-    postDate: [2023,3,10, 11, 52],
+    img: "Avengers.png",
+    age: "3min",
     user: "eric",
     likes: 43,
     message: "Los miembros supervivientes de Los Vengadores y sus aliados intentan revertir el daño causado por Thanos en Infinity War.",
     comments: 15
   },
   {
-    img: "Matrix.jpg",
-    postDate: [2023,3,15, 2, 16],
+    img: "Spiderman.png",
+    age: "16hour",
+    user: "david",
+    likes: 18,
+    message: "Tras 8 meses de la muerte de su mentor Tony Stark, Peter Parker decide pasar unas merecidas vacaciones en Europa junto a sus amigos Ned Leeds y Michelle; pero sus planes al final se ven truncados cuando Nick Fury le encomienda una nueva misión",
+    comments: 12
+  },
+  {
+    img: "Matrix.png",
+    age: "2day",
     user: "pamela",
     likes: 35,
     message: "Matrix es una tetralogía de películas de ciencia ficción escritas y dirigidas por las hermanas Wachowski. Se compone de The Matrix (1999), The Matrix Reloaded (2003), The Matrix Revolutions (2003) y The Matrix Resurrections (2021)",
     comments: 9
-  },
-  {
-    img: "Spiderman.jpg",
-    postDate: [2023,3,25, 14, 20],
-    user: "david",
-    likes: 18,
-    message: "Posteriormente a los eventos de Avengers: Endgame,2​3​ y tras 8 meses de la muerte de su mentor Tony Stark, Peter Parker decide pasar unas merecidas vacaciones en Europa junto a sus amigos Ned Leeds y Michelle; pero sus planes al final se ven truncados cuando Nick Fury le encomienda una nueva misión",
-    comments: 12
   }
 ];
 
 function PostList() {
   return (
-    <div className="row gx-5 gy-3 mx-auto">
+    <div className="row gx-5 gy-3 mx-auto mt-2 mb-3">
       {posts.map((post, i) => (
         <Post
           key={i}
           img={post.img}
-          postDate={post.postDate}
+          age={post.age}
           user={post.user}
           likes={post.likes}
           message={post.message}
