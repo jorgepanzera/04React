b154e4f0ee7e1da80f6492883e754dc8a40f362d
Jorge <panzera.jorge@gmail.com>
2023-03-30 21:34:06 -0300

    working on searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './components/NavBar'
 import SearchBar from './components/SearchBar';
 import PostList from './components/PostList';
@@ -10,12 +10,17 @@ function App() {
 
   // State
   const [section, setSection] = useState('Normal')
+  const [search, setSearch] = useState('')
+  const [firstLoad, setfirstLoad] = useState(true)
+
+  console.log(`App ${search}`)
 
   // Props para hijos
   const profileAvatar = require('./assets/images/profile.PNG')
   const username = "jpanzera"
   const bio = "Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi"
 
+  // Funciones que se usan desde componente NavBar
   const onLogoClick = () => {
     setSection('Normal')
   }
@@ -24,14 +29,31 @@ function App() {
     setSection('Profile')
   }
 
+  // Funciones que se usan desde componente SearchBar
+  const onSearch = (searchString) => {
+    setSearch(searchString)
+  }
 
-  // Render condicional a lo que quiero mostrar
+  
+  // Effect para cargar los posts a los 3 segundos y mostrar los post
+  // usar los 3 segundos solamente con firstLoad = true
+  /*
+    if firstload
+       espero 3 segundos
+       setPosts
+       firstload = false
+    else
+      setPost - viene de searchbar 
+  */
+
+
+  // Render condicional 
 
   if (section === 'Normal') {
     return (
       <div className="App">
         <NavBar onLogoClick={onLogoClick} onProfileClick={onProfileClick} />
-        <SearchBar />
+        <SearchBar value={search} onSearch={onSearch} />
         <PostList />
       </div>
     )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,13 @@
-function SearchBar() {
+function SearchBar({value, onSearch}) {
+
+    const handleInput = () => {
+      let q = document.getElementById("form-search")
+      onSearch(q.value)
+    }
+
     return (
       <div className="container form-outline mt-2">
-        <input type="search" id="form-search" className="form-control" placeholder="Search" aria-label="Search" />
+        <input type="search" id="form-search" className="form-control" placeholder="Search" defaultValue={value} onInput={handleInput} aria-label="Search" />
       </div>
     );
   }
