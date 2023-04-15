184871dd14aa08d2bc223bc73670bae05669905c
Jorge <panzera.jorge@gmail.com>
2023-04-14 21:17:26 -0300

    getPayload

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -4,8 +4,6 @@ import data from "../data/profiles.json";
 export async function getToken(username, password) {
   try {
 
-    console.log("entre getToken")
-
     const headers = {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin':'*'
@@ -17,16 +15,44 @@ export async function getToken(username, password) {
       `{"username":"${username}", "password":"${password}"}`,
       { headers: headers }
     );
+    
     if (response.data.token) {
       localStorage.setItem("myThreePicsToken", response.data.token);
       return true;
+    } else {
+      return false;
     }
-    return false;
+    
   } catch (error) {
     return(false)
   }
 }
 
+/* Usuario creado desde Postman password P4ssW0rd!#
+{
+    "avatar": "https://res.cloudinary.com/dvjb4do4c/image/upload/v1681516447/three-points/redzzjuwnqfchrve6fro.png",
+    "username": "@jorge.panzera",
+    "bio": "Hello World",
+    "name": "Jorge Panzera",
+    "createdAt": "2023-04-14T23:54:08.284Z",
+    "updatedAt": "2023-04-14T23:54:08.284Z",
+    "id": "6439e7a097549600147adffd"
+}
+*/
+
+
 export function getProfile() {
+  console.log(getPayload(localStorage.getItem("myThreePicsToken")))
   return data;
+}
+
+
+// Obtener el Payload del token para obtener el user id (sub)
+//{sub: '6136944fcd79ba24707e2f82', exp: 1681521250, iat: 1681517650}
+function getPayload(token) {
+  if (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(atob(base64));
+  }
 }
